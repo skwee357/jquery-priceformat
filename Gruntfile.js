1f32386d7c60533002e614eabcc1798c6b521db5
skwee357 <ko.dmitri@gmail.com>
2014-10-01 18:56:02 +0300

    Rearranged examples, added functionality of signed or unsigned numbers, bower package, switched fully to grunt releasing

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,8 @@ module.exports = function (grunt) {
             options: {
                 files: ['package.json', 'priceformat.jquery.json', 'bower.json'],
                 updateConfigs: ['pkg'],
-                commit: false,
+                commit: true,
+                "commitMessage": "Release v%VERSION%",
                 createTag: true,
                 tagName: '%VERSION%',
                 tagMessage: 'Release v%VERSION%',
@@ -35,6 +36,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('release:patch', ['bump-only:patch', 'uglify', 'bump-commit']);
-    grunt.registerTask('release:minor', ['bump-only:patch', 'uglify', 'bump-commit']);
-    grunt.registerTask('release:major', ['bump-only:patch', 'uglify', 'bump-commit']);
+    grunt.registerTask('release:minor', ['bump-only:minor', 'uglify', 'bump-commit']);
+    grunt.registerTask('release:major', ['bump-only:major', 'uglify', 'bump-commit']);
 };
